c42aa905ded2839ff740b1c9d2b11baed5e1a8e7
Shivam <shivam27777@gmail.com>
2024-02-01 00:54:11 +0530

    Initialized FE and BE and Environment

---
+++ backend/db.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+const MongoUrl = process.env.MONGO_DB_URL;
+
+mongoose.connect(MongoUrl)
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+});
+
+// Create a model from the schema
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+	User
+};
