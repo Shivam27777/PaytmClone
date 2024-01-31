7e7c9f26739082d5bfb4b4b93019c84f5530e39b
Shivam <shivam27777@gmail.com>
2024-02-01 01:24:42 +0530

    added JWT token, cors and body parser for api

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
-const mainRouter = require("./routes/index");
+const rootRouter = require("./routes/index");
+const cors = require("cors");
 
 const app = express();
 
-app.use("api/v1",mainRouter);
+app.use("api/v1",rootRouter);
 
+//since FE and BE are one some machine
+app.use(cors());
+app.use(express.json());
+
+app.listen(3000);
